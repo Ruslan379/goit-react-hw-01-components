eb85989aab62ee859dcef222ab46c9edadf9bbf9
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-06 14:40:14 +0300

    Work on 2-1

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; //! TEMP
+// import ReactDOM from 'react-dom'; //! TEMP
 
 
-// import ReactDOM from 'react-dom/client'; //! Нужен ОБЯЗАТЕЛЬНО!
+import ReactDOM from 'react-dom/client'; //! Нужен ОБЯЗАТЕЛЬНО!
 // import { App } from 'components/App'; //? old path
 import { App } from 'components/App/App';
 import './index.css';
 
 //! Нужен ОБЯЗАТЕЛЬНО!
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 //! +++++++++++++++++++++++++++++++++++++++++++
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+//! TEMP
+// ReactDOM.render(<App />, document.querySelector("#root"));
 
