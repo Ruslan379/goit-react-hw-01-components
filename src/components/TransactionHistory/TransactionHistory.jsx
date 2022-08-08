8222c45fc23476c3d044ec0a648f0310de27c115
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-08 16:27:12 +0300

    Finished  styling Transaction with Emotion  - 12

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,34 +1,42 @@
 import PropTypes from 'prop-types';
 
-import css from './TransactionHistory.module.css'
+// import css from './TransactionHistory.module.css' //? OLD, before creating the file jsconfig.json
+// import css from 'components/TransactionHistory/TransactionHistory.module.css' //todo = старый вариант импорта стилей
+//! НОВЫЙ вариант импорта стилей
+import { TransactionsHistory } from 'components/TransactionHistory/TransactionHistory.styled'; 
+import { ItemHistory } from 'components/TransactionHistory/TransactionHistory.styled'; 
+import { TableTh } from 'components/TransactionHistory/TransactionHistory.styled'; 
+import { TableTd } from 'components/TransactionHistory/TransactionHistory.styled'; 
 
 
+
+//! +++++++++++++++ НОВЫЙ вариант импорта стилей с TransactionHistory.styled.jsx ++++++++++++++++++
 export function TransactionHistory({ title, items = [] }) {
     // console.log(items[0]); //!
     return (
-        <section className={css.transactionHistory}>
-        {title && <h2>{title}</h2>}
-            
-        <table className={css.itemHistory}>
-            <thead>
-                <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
-                </tr>
-            </thead>
+        <TransactionsHistory>
+            {title && <h2>{title}</h2>}
 
-            <tbody>
-                {items.map(item => (
-                    <tr key={item.id}>
-                        <td>{item.type}</td>
-                        <td>{item.amount}</td>
-                        <td>{item.currency}</td>
+            <ItemHistory>
+                <thead>
+                    <tr>
+                        <TableTh>Type</TableTh>
+                        <TableTh>Amount</TableTh>
+                        <TableTh>Currency</TableTh>
                     </tr>
-                ))}
-            </tbody>
-        </table>
-        </section>
+                </thead>
+
+                <tbody>
+                    {items.map(item => (
+                        <tr key={item.id}>
+                            <TableTd>{item.type}</TableTd>
+                            <TableTd>{item.amount}</TableTd>
+                            <TableTd>{item.currency}</TableTd>
+                        </tr>
+                    ))}
+                </tbody>
+            </ItemHistory>
+        </TransactionsHistory>
     );
 };
 
@@ -48,3 +56,33 @@ TransactionHistory.propTypes = {
 
 
 
+
+//todo  ---- старый вариант импорта стилей c TransactionHistory.module.css ----------
+// export function TransactionHistory({ title, items = [] }) {
+//     // console.log(items[0]); //!
+//     return (
+//         <section className={css.transactionHistory}>
+//         {title && <h2>{title}</h2>}
+            
+//         <table className={css.itemHistory}>
+//             <thead>
+//                 <tr>
+//                     <th>Type</th>
+//                     <th>Amount</th>
+//                     <th>Currency</th>
+//                 </tr>
+//             </thead>
+
+//             <tbody>
+//                 {items.map(item => (
+//                     <tr key={item.id}>
+//                         <td>{item.type}</td>
+//                         <td>{item.amount}</td>
+//                         <td>{item.currency}</td>
+//                     </tr>
+//                 ))}
+//             </tbody>
+//         </table>
+//         </section>
+//     );
+// };
--- src/components/TransactionHistory/TransactionHistory.styled.jsx
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -1,2 +1,40 @@
 import styled from '@emotion/styled';
 
+export const TransactionsHistory = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 300px;
+    padding: 40px;
+    margin-right: 20px;
+
+    border: 2px solid rgb(61, 61, 61);
+    border-radius: 20px;
+    box-shadow: 10px 10px 14px -3px rgba(0, 0, 0, 0.43);
+
+    background-color: rgb(254, 255, 221);
+`;
+
+
+export const ItemHistory = styled.table`
+    border-collapse: collapse;
+    text-transform: capitalize;
+    text-align: center;
+    font-size: 12px;
+    box-shadow: 10px 10px 14px -3px rgba(0, 0, 0, 0.43);
+`;
+
+
+export const TableTh = styled.th`
+    padding: 5px;
+    border: 1px solid #2a2a2a;
+    background-color: rgb(205, 232, 255);
+`;
+
+
+export const TableTd = styled.td`
+    padding: 5px;
+    border: 1px solid #2a2a2a;
+    background-color: rgb(250, 250, 235);
+`;
