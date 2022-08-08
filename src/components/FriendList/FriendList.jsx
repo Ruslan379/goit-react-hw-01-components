2e730e447e053f5efde404a938e3b57f4774a799
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-08 15:00:22 +0300

    Work on styling FriendList with Emotion - 8

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,31 +1,51 @@
 import PropTypes from 'prop-types';
 
-import css from './FriendList.module.css'
+// import css from './FriendList.module.css' //? OLD, before creating the file jsconfig.json
+// import css from 'components/FriendList/FriendList.module.css' //todo = старый вариант импорта стилей
+//! НОВЫЙ вариант импорта стилей
+import { FriendsList } from 'components/FriendList/FriendList.styled'; 
+import { StatList } from 'components/FriendList/FriendList.styled'; 
+import { Item } from 'components/FriendList/FriendList.styled'; 
+import { Status } from 'components/FriendList/FriendList.styled'; 
+import { Avatar } from 'components/FriendList/FriendList.styled';
+import { Name } from 'components/FriendList/FriendList.styled';
 
 
 
+//! +++++++++++++++ НОВЫЙ вариант импорта стилей с FriendList.styled.jsx ++++++++++++++++++
 export function FriendList({ title, friends = [] }) {
     return (
-        <section className={css.friendList}>
+        <FriendsList>
         {title && <h2>{title}</h2>}
 
-        <ul className={css.statList}>
+        <StatList>
             {friends.map(friend => (
-                <li key={friend.id} className={css.item} style={{сolor: (friend.isOnline ? "#b30000" : "#009100"), backgroundColor: (friend.isOnline ? "#009100" : "#b30000")}}>
-                    <span className={css.status} style={{сolor: (friend.isOnline ? "#b30000" : "#009100")}}>{friend.isOnline ? 'On' : 'Off'}</span>
-                    <img
-                        className={css.avatar}
+                <Item
+                    key={friend.id} 
+                    style={{ сolor: (friend.isOnline ? "#b30000" : "#009100"), backgroundColor: (friend.isOnline ? "#009100" : "#b30000") }}
+                >
+                    <Status
+                        style={{ сolor: (friend.isOnline ? "#b30000" : "#009100") }}
+                    >
+                        {friend.isOnline ? 'On' : 'Off'}
+                    </Status>
+                    <Avatar
                         src={friend.avatar}
                         alt="User avatar"
                         width="48"
                     />
-                    <p className={css.name} style={{сolor: (friend.isOnline ? "#b30000" : "#009100")}}>{friend.name}</p>
-                </li>
+                    <Name
+                        style={{ сolor: (friend.isOnline ? "#b30000" : "#009100") }}
+                    >
+                        {friend.name}
+                    </Name>
+                </Item>
             ))}
-            </ul>
-        </section>
+            </StatList>
+        </FriendsList>
     );
 };
+
 
 
 FriendList.propTypes = {
@@ -41,4 +61,39 @@ FriendList.propTypes = {
 };
 
 
+
+
+//todo  ---- старый вариант импорта стилей c FriendList.module.css ----------
+// export function FriendList({ title, friends = [] }) {
+//     return (
+//         <section className={css.friendList}>
+//         {title && <h2>{title}</h2>}
+
+//         <ul className={css.statList}>
+//             {friends.map(friend => (
+//                 <li
+//                     className={css.item}
+//                     key={friend.id} 
+//                     style={{ сolor: (friend.isOnline ? "#b30000" : "#009100"), backgroundColor: (friend.isOnline ? "#009100" : "#b30000") }}
+//                 >
+//                     <span className={css.status}
+//                         style={{ сolor: (friend.isOnline ? "#b30000" : "#009100") }}>{friend.isOnline ? 'On' : 'Off'}
+//                     </span>
+//                     <img
+//                         className={css.avatar}
+//                         src={friend.avatar}
+//                         alt="User avatar"
+//                         width="48"
+//                     />
+//                     <p className={css.name}
+//                         style={{ сolor: (friend.isOnline ? "#b30000" : "#009100") }}
+//                     >
+//                         {friend.name}
+//                     </p>
+//                 </li>
+//             ))}
+//             </ul>
+//         </section>
+//     );
+// };
 
--- src/components/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList.styled.jsx
@@ -1,2 +1,82 @@
 import styled from '@emotion/styled';
 
+export const FriendsList = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 300px;
+    padding: 40px;
+    margin-right: 20px;
+
+    /* text-transform: uppercase; */
+
+    border: 2px solid rgb(61, 61, 61);
+
+    border-radius: 20px;
+    box-shadow: 10px 10px 14px -3px rgba(0, 0, 0, 0.43);
+    background-color: rgb(254, 255, 221);
+`;
+
+
+export const StatList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    padding: 0;
+
+    list-style-type: none;
+    text-align: center; 
+
+    border: 1px solid rgb(0, 0, 0);
+    border-right: 2px solid rgb(0, 0, 0);
+    border-radius: 2px;
+    box-shadow: 10px 10px 14px -3px rgba(0, 0, 0, 0.43);
+`;
+
+
+export const Item = styled.li`
+    display: flex;
+    align-items: center;
+
+    padding: 10px 36px;
+    
+    border-right: 1px solid rgb(0, 0, 0);
+    border-bottom: 2px solid rgb(0, 0, 0);
+    /* background-color: rgb(199, 199, 199); */
+
+
+
+`;
+
+
+export const Status = styled.span`
+    margin-right: 15px;
+
+    font-size: 14px;
+    line-height: 32px;
+    letter-spacing: 0.5px;
+
+    color: rgb(255, 50, 0);
+`;
+
+export const Avatar = styled.img`
+    border: 1px solid rgb(61, 61, 61);
+    padding: 15px;
+    border-radius: 50%;
+    background-color: rgb(205, 232, 255);
+    box-shadow: 10px 10px 14px -3px rgba(0, 0, 0, 0.43);
+`;
+
+
+export const Name = styled.p`
+    margin-left: 28px;
+
+    font-size: 24px;
+    line-height: 24px;
+    font-weight: 700;
+    letter-spacing: 0.5px;
+
+    color: rgb(255, 240, 0);
+`;
