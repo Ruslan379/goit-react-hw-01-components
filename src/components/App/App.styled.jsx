0f728138fb492ef398cbce053337bc5bac86c689
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-10-07 16:49:22 +0300

    Исправлены все замечания Ментора+normalize+отступы-3

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -2,5 +2,8 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   display: flex;
+  justify-content: space-between;
+  // justify-content: space-around;
+  // justify-content: space-evenly;
   // display: block;
 `;
